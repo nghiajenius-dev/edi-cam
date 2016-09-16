270e66b979a34757a81dd4e9a859be071a44a724
nghiajenius-dev <bknghiajenius@gmail.com>
2016-09-16 17:17:04 +0700

    - Higher resolution: 640 x480 @30Hz, bitrate: 3200k

--- bin/do_ffmpeg.sh
+++ bin/do_ffmpeg.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-/home/root/bin/ffmpeg/ffmpeg -s 320x240 -f video4linux2 -i /dev/video0 -f mpeg1video \
--b 800k -r 30 http://127.0.0.1:8082
-
+# default: WxH (4:3) = 320x240, bitrate: -b 800k 
+# higher resolution test (4:3)
+# - 640x480, 1600k  --> ok
+# - 640x480, 3200k  --> ok, little lagging
+/home/root/bin/ffmpeg/ffmpeg -s 640x480 -f video4linux2 -i /dev/video0 -f mpeg1video \
+-b 3200k -r 30 http://127.0.0.1:8082
